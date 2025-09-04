47d3b039d861c780ff0be6705a0acfcce0ace432
YudTakeda <YudTakeda@mx1.wiseman.co.jp>
2025-09-04 10:02:43 +0900

    Update yml & stack

--- my-cdk-weatherapp/lib/my-cdk-weatherapp-stack.ts
+++ my-cdk-weatherapp/lib/my-cdk-weatherapp-stack.ts
@@ -3,7 +3,6 @@ import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
-import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
 
 export class WeatherApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,7 +10,7 @@ export class WeatherApiStack extends cdk.Stack {
 
     // 1. VPC作成
     const vpc = new ec2.Vpc(this, 'WeatherApiVpc', {
-      maxAzs: 2, // 可用ゾーン2つ
+      maxAzs: 2,
     });
 
     // 2. ECSクラスタ作成
@@ -19,19 +18,17 @@ export class WeatherApiStack extends cdk.Stack {
       vpc,
     });
 
-    // 3. Dockerイメージをビルド（ローカルDockerfileから）
-    const dockerImageAsset = new ecr_assets.DockerImageAsset(this, 'WeatherApiImage', {
-      directory: '../weatherSearch', // Dockerfile のあるディレクトリ
-    });
-
-    // 4. Fargateサービス + ALB を作成
+    // 3. Fargateサービス + ALB を作成 (ECR イメージを参照)
     new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'WeatherApiService', {
       cluster,
       taskImageOptions: {
-        image: ecs.ContainerImage.fromDockerImageAsset(dockerImageAsset), // CDK がビルド&ECR push したイメージを利用
+        image: ecs.ContainerImage.fromRegistry(
+          `${cdk.Aws.ACCOUNT_ID}.dkr.ecr.${cdk.Aws.REGION}.amazonaws.com/weatherrepo:latest`
+        ),
         containerPort: 3000,
       },
-      publicLoadBalancer: true, // インターネットからアクセス可能
+      publicLoadBalancer: true,
     });
   }
 }
+
